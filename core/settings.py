47d68fa82ea84c824a65697dd43500a7cc27e2f5
Christopher75 <nigerianstudentassociationcuu@gmail.com>
2026-05-01 20:30:29 +0300

    Fix CSRF trusted origins

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-university-fresh-2026')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
-
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
